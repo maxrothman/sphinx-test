5ac375c448e175d702e8bacac84c121d60431746
Max Rothman <max@edx.org>
2016-04-18 22:51:10 -0400

    Add some docs

--- code/translator.py
+++ code/translator.py
@@ -1,6 +1,16 @@
 from sphinx.writers import html
 
 class NonCodeSmartyPantsHTMLTranslator(html.SmartyPantsHTMLTranslator):
+  '''
+  Makes desc_name nodes render as <span> instead of <code> tags
+
+  The default Sphinx behavior is to output the names of all domain objects
+  inside <code> tags (from writers/html.py@HTMLTranslator). Since we are not
+  documenting code, <span> tags are a better choice.
+
+  This subclasses SmartyPantsHTMLTranslator so as to also include the
+  SmartyPants functionality (--- em-dash, etc.)
+  '''
   def visit_desc_name(self, node):
     self.body.append(self.starttag(node, 'span', '', CLASS='descname'))
 
